66873cbaf564c587392e14a6c02690d3df9d6f49
sorines <sarah.orines@gmail.com>
2018-03-27 23:48:07 -0700

    I had to use win lose on that. It was not pretty

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,26 +2,8 @@
 //id=answer, the answers appear after the 20 seconds are up
 //you can only pick one answer for ever question
 //the page will reveal the number of answers that are correct or wrong
- 
-var answers = {
-        movie:"Varsity Blues", 
-        Diwali:"Hindu Hallowing",
-        Cat:"Sprinkles",
-        Actress:"Hillary Swank"
-    
-};
 
-$( "input[type=radio][name=radio]:checked" ).val();
-
-var correct = val();
-
-
-console.log(answers.movie);
-console.log("#result");
-
-
-
-
+//this sets answers
 
 
 //countdown
@@ -48,7 +30,27 @@ function stop(){
 
 };
 
+console.log(timers);
 console.log(timer);
-console.log("#timer");
 
 timers();
+
+//trying to get this to answer at random trivia. It comes out at
+//correct answers. win, lose. 
+clearInterval(intervalId);
+
+if  ($("input").val(["answer"])){
+    $("#result").text("Win,Win..Win");
+    clearInterval(intervalId);
+}else{
+($("input").val(["wrong"]));
+$("#result").text("False, Brown Bear.");
+
+}
+
+console.log("input");
+console.log("#result");
+
+
+
+
